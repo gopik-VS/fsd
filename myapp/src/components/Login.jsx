dfb994584fcb5abf9f445932c6f2d00ed21b8f3a
gopika-vs <144515688+gopik-VS@users.noreply.github.com>
2024-10-29 14:27:10 +0530

    Add files via upload

---
+++ myapp/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h1>Login</h1>
+        <input type="text" name=" " id="" placeholder="Enter your Username" /><br /><br />
+        <input type="password" name="" id="" label='password' placeholder='password' />
+    </div>
+  )
+}
+
+export default Login
---
+++ myapp/src/components/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+        <h1>signup</h1>
+        <input type="text" name=" " id="" placeholder="Enter your Username" /><br /><br />
+        <input type="email" name="" id=""  placeholder="Enter your email address" /> <br /><br />
+        <input type="number" name="" id="" placeholder="Enter your phone number" /> <br /><br />
+        <input type="password" name="" id="" label='password' placeholder='password' /><br /><br />
+        <button type="submit">submit</button>
+    </div>
+  )
+}
+
+export default Signup
